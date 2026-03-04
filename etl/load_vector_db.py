d8533cc517a4c78f0f1053ee65b804cc8d38fb10
Yash Janbandhu <yashjanbandhu12@gmail.com>
2026-03-04 20:20:54 +0530

    Add vector database creation using Chroma embeddings

---
+++ etl/load_vector_db.py
@@ -0,0 +1,23 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def create_vector_db(chunks):
+
+    texts = [c["content"] for c in chunks]
+
+    metadata = [{"page": c["page"]} for c in chunks]
+
+    vectordb = Chroma.from_texts(
+        texts=texts,
+        embedding=OpenAIEmbeddings(),
+        metadatas=metadata,
+        persist_directory="db"
+    )
+
+    vectordb.persist()
+
+    return vectordb
