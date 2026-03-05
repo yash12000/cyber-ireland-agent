585a623e330998d379b7a1f67585e5bf314efa26
Yash Janbandhu <yashjanbandhu12@gmail.com>
2026-03-05 18:00:05 +0530

    Clean previous vector database to regenerate with local embeddings

--- app/retriever.py
+++ app/retriever.py
@@ -1,12 +1,16 @@
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
 
 
 def get_retriever():
 
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
     vectordb = Chroma(
         persist_directory="db",
-        embedding_function=OpenAIEmbeddings()
+        embedding_function=embeddings
     )
 
     retriever = vectordb.as_retriever(
