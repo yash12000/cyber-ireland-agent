c9ea7c967bc1228d3d4d83ca66cb4af0eb092434
Yash Janbandhu <yashjanbandhu12@gmail.com>
2026-03-05 13:54:30 +0530

    Replace OpenAI embeddings with local HuggingFace embeddings

--- etl/load_vector_db.py
+++ etl/load_vector_db.py
@@ -4,22 +4,21 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def create_vector_db(chunks):
 
-    print("Loading embedding model...")
-
-    embeddings = HuggingFaceEmbeddings(
+    embedding = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
     texts = [c["content"] for c in chunks]
+
     metadata = [{"page": c["page"]} for c in chunks]
 
     vectordb = Chroma.from_texts(
         texts=texts,
-        embedding=embeddings,
+        embedding=embedding,
         metadatas=metadata,
         persist_directory="db"
     )
 
     vectordb.persist()
 
-    print("Vector DB created successfully!")
+    return vectordb
